afc8d97b279e9ff0bddd05aa3ab3f8030a4815f4
Alexandru Ursu <bodhipankh@yahoo.com>
2023-01-02 04:12:59 +0000

    Add division code in js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,9 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
 
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`; 
@@ -88,6 +91,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === '-') {
         return [operand1 - operand2, "subtract"]
+    } else if (operator === '/') {
+        return [Math.floor(operand1 / operand2), "division"]
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`; 
@@ -128,5 +133,11 @@ function displayMultiplyQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";
+    document.getElementById('operator').textContent = "x"; 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = (operand1 * operand2);
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
